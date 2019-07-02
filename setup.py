8c6b2e4fff8c103cb092931935844f5633030826
Adi Suissa-Peleg <adi.suissa@gmail.com>
2019-07-02 14:18:13 +0000

    Adding the link to the required gcsfs repo

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
 #         ],
     name="rh_img_access_layer",
     packages=["rh_img_access_layer"],
+    dependency_links = ['http://github.com/adisuissa/gcsfs/tarball/master#egg=fs_gcsfs-0.4.1'],
     url="https://github.com/Rhoana/rh_img_access_layer",
     version="0.0.1"
 )
